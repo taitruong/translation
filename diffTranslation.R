b41763004362e4bdfcdff84cfc84b151eb67e539
taitruong <maitai.truong@gmail.com>
2015-04-08 16:16:00 +0200

    do not define columns for empty data frame, improve performance

--- diffTranslation.R
+++ diffTranslation.R
@@ -1,16 +1,14 @@
 diffTranslation <- function(current, latest, colText = 'Text') {
+        # merge current and latest data frame by id and
+        # add suffix for all redundant columns in both data frames with '.current' and '.latest'
         m <- merge(current, latest, by = 'ID', suffixes=c('.current', '.latest'))
         
-        result <- data.frame(
-                ID=numeric(),
-                Text.current=character(),
-                Text.latest=character())
+        result <- data.frame()
         count <- 1
         for (i in 1:nrow(m)) {
-                current <- m[[i, paste(colText, '.current', sep ='')]]
-                latest <- m[[i, paste(colText, '.latest', sep ='')]]
-                #print(class(current))
-                if (current != latest) {
+                # colText + '.current' != colText + '.latest' ???
+                # if different then add to result
+                if (m[[i, paste(colText, '.current', sep ='')]] != m[[i, paste(colText, '.latest', sep ='')]]) {
                         #print(m[i,])
                         result <- rbind(result, m[i,])
                         #result[count,] <- m[i,]
