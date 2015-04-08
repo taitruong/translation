8b60535c1cd9bc093e9cd6a9d2fe893391928751
taitruong <maitai.truong@gmail.com>
2015-04-08 17:03:30 +0200

    suffix only for latest/second data frame needed

--- diffTranslation.R
+++ diffTranslation.R
@@ -1,14 +1,14 @@
 diffTranslation <- function(current, latest, colText = 'Text') {
         # merge current and latest data frame by id and
         # add suffix for all redundant columns in both data frames with '.current' and '.latest'
-        m <- merge(current, latest, by = 'ID', suffixes=c('.current', '.latest'))
+        m <- merge(current, latest, by = 'ID', suffixes=c('', '.latest'))
         
         result <- data.frame()
         count <- 1
         for (i in 1:nrow(m)) {
-                # colText + '.current' != colText + '.latest' ???
+                # colText != colText + '.latest' ???
                 # if different then add to result
-                if (m[[i, paste(colText, '.current', sep ='')]] != m[[i, paste(colText, '.latest', sep ='')]]) {
+                if (m[[i, colText]] != m[[i, paste(colText, '.latest', sep ='')]]) {
                         #print(m[i,])
                         result <- rbind(result, m[i,])
                         #result[count,] <- m[i,]
